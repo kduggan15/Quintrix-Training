2289457b9d95fc9714a8d8799c563bb932629cb7
Kieran <KieranDuggan15@gmail.com>
2020-10-28 18:58:40 -0400

    Homework 1

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 public class NewTest {
 	  @Test
 	  public void f() {
+		  System.out.println("HelloWorld");
 	  }
 	  @BeforeTest
 	  public void beforeTest() {
