c1d0391572202477953874579cb3c44d9f41d3f3
Kieran <KieranDuggan15@gmail.com>
2020-11-12 21:39:06 -0500

    Adds form authentication test class

---
+++ src/test/java/TheInternet/tests/FormAuthenticationTest.java
@@ -0,0 +1,22 @@
+package TheInternet.tests;
+
+import org.testng.annotations.Test;
+
+import TheInternet.foundation.TestBase;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class FormAuthenticationTest extends TestBase{
+  @Test
+  public void verifiesCanLogin() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
