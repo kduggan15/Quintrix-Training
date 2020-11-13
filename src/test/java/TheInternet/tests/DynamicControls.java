35af6c47d2f5ab3fe783c3afa7922f624213f0fb
Kieran <KieranDuggan15@gmail.com>
2020-11-12 20:01:15 -0500

    Adds test for checkbox on dynamic controls page

---
+++ src/test/java/TheInternet/tests/DynamicControls.java
@@ -0,0 +1,36 @@
+package TheInternet.tests;
+
+import org.testng.annotations.Test;
+
+import TheInternet.foundation.TestBase;
+import TheInternet.pages.DynamicControlsPage;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+
+public class DynamicControls extends TestBase{
+  @Test
+  public void verfiesCheckboxLoads() {
+	  //Arrange
+	  //boolean isCheckBoxPresent;
+	  //Act
+	  boolean isCheckboxPresent = new DynamicControlsPage(driver, baseURL)
+			  .navigate()
+			  .removeCheckbox()
+			  .waitForNoCheckBox()
+			  .isCheckboxPresent();
+	  //Assert
+	  Assert.assertFalse(isCheckboxPresent);
+  }
+  @BeforeTest
+  public void beforeTest() {
+	  super.beforeTest();
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  super.afterTest();
+  }
+
+}
