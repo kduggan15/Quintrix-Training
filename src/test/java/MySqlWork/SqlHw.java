165287b47b163ab1e8000ae822e209e255e7df7d
Kieran <KieranDuggan15@gmail.com>
2020-11-21 10:26:20 -0500

    Updates 7

--- src/test/java/MySqlWork/SqlHw.java
+++ src/test/java/MySqlWork/SqlHw.java
@@ -34,6 +34,7 @@ public class SqlHw {
 		this.user = "root";
 		this.password = configs.get("dbpass");
 	}
+	
 	@Test
 	public void Problem2() {
 		
@@ -50,8 +51,44 @@ public class SqlHw {
 		} catch (SQLException ex) {
 			System.out.println(ex);
 		} 
-
 	}
+	
+	@Test
+	public void Problem7() {
+		//String query = "SELECT inventory_id FROM store INNER JOIN inventory INNER JOIN film WHERE title=\"Alien Center\" AND store.store_id=2;";
+		try {
+			Connection con = DriverManager.getConnection(url, user, this.password);
+			Statement st = con.createStatement();
+			st.executeQuery("SET @filmId = (SELECT f.film_id \n"
+					+ "FROM film AS f\n"
+					+ "WHERE f.title = 'Alien Center')");
+			st.executeQuery("SET @filmCount = 0");
+			st.executeQuery("CALL film_in_stock(@filmId, 2, @filmCount)");
+			ResultSet rs = st.executeQuery("SELECT @filmCount;");
+			if (rs.next()) {
+				System.out.println(rs.getString(1));
+				Assert.assertTrue(rs.getString(1).contains("4"));
+			}
+
+		} catch (SQLException ex) {
+			System.out.println(ex);
+		} 
+	}
+	
+	@Test
+	public void Problem8_9_10(){
+		try {
+			Connection con = DriverManager.getConnection(url, user, this.password);
+			con.setAutoCommit(false);
+			Statement st = con.createStatement();
+			st.executeQuery("INSERT INTO city(city, country_id) VALUES('New City', 103);");
+			con.rollback();
+
+		} catch (SQLException ex) {
+			System.out.println(ex);
+		} 
+	}
+	
 	@BeforeTest
 	public void beforeTest() {
 	}
